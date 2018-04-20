dd44adf7f63cb689635d7c70edb68405b54b6e85
Matt Bruder <mbruder@students.kennesaw.edu>
2018-04-20 19:19:14 -0400

    first push

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.ue9wcRPia0rkQ1PQmZ8ftcxNP,
+  consumer_secret: process.env.SMLZEdNsANrbzrdO7xe6sS0rkUX8wdyfll0sMEZQNWDg60o4qw,
+  access_token_key: process.env.BDvGC4YwPZsTX6iVBnFF8PwOmWpNbqE,
+  access_token_secret: process.env.FybQvnxVOxa2gUBFfImDcJizqSPq4zt1BRsBNRD5YAgWs
+
+};
+
+exports.spotify = {
+  id: process.env.c8429e610e9349758f2e1ad48ca3ea6a,
+  secret: process.env.c6c8c439a01f48cab4ae7c00e69188e0
+};
